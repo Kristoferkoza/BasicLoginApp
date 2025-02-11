1f297ff10a9ee34fe7227f20febee71572af5bc1
kfkzkozakiewicz@gmail.com <101817983+Kristoferkoza@users.noreply.github.com>
2025-02-11 20:39:47 +0100

    frontend-files-structure-built

--- bl-frontend/src/app/app.routes.ts
+++ bl-frontend/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AppPageComponent } from './app-page/app-page.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'first-page',
+        component: FirstPageComponent,
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+    },
+    {
+        path: 'app-page',
+        component: AppPageComponent,
+    },
+    {
+        path: '',
+        redirectTo: '/first-page',
+        pathMatch: 'full',
+    }
+];
