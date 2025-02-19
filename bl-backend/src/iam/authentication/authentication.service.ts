4fd1139c40f1ea64a9109cf9aac8f211cf5abf1e
kfkzkozakiewicz@gmail.com <101817983+Kristoferkoza@users.noreply.github.com>
2025-02-19 08:59:14 +0100

    refresh-tokens-added

--- bl-backend/src/iam/authentication/authentication.service.ts
+++ bl-backend/src/iam/authentication/authentication.service.ts
@@ -1,74 +1,111 @@
 import {
-    ConflictException,
-    Inject,
-    Injectable,
-    UnauthorizedException,
-  } from '@nestjs/common';
-  import { InjectRepository } from '@nestjs/typeorm';
-  import { Repository } from 'typeorm';
-  import { User } from '../../users/entities/user.entity';
-  import { HashingService } from '../hashing/hashing.service';
-  import { SignInDto } from './dto/sign-in.dto';
-  import { SignUpDto } from './dto/sign-up.dto';
+  ConflictException,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { HashingService } from '../hashing/hashing.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
 import { JwtService } from '@nestjs/jwt';
 import jwtConfig from '../config/jwt.config';
 import { ConfigType } from '@nestjs/config';
 import { ActiveUserData } from '../interfaces/active-user-data.interface';
-  
-  @Injectable()
-  export class AuthenticationService {
-    constructor(
-      @InjectRepository(User) private readonly usersRepository: Repository<User>,
-      private readonly hashingService: HashingService,
-      private readonly jwtService: JwtService,
-      @Inject(jwtConfig.KEY)
-      private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
-    ) {}
-  
-    async signUp(signUpDto: SignUpDto) {
-      try {
-        const user = new User();
-        user.email = signUpDto.email;
-        user.password = await this.hashingService.hash(signUpDto.password);
-  
-        await this.usersRepository.save(user);
-      } catch (err) {
-        const pgUniqueViolationErrorCode = '23505';
-        if (err.code === pgUniqueViolationErrorCode) {
-          throw new ConflictException();
-        }
-        throw err;
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.usersRepository.save(user);
+    } catch (err) {
+      const pgUniqueViolationErrorCode = '23505';
+      if (err.code === pgUniqueViolationErrorCode) {
+        throw new ConflictException();
       }
-    }
-  
-    async signIn(signInDto: SignInDto) {
-      const user = await this.usersRepository.findOneBy({
-        email: signInDto.email,
-      });
-      if (!user) {
-        throw new UnauthorizedException('User does not exists');
-      }
-      const isEqual = await this.hashingService.compare(
-        signInDto.password,
-        user.password,
-      );
-      if (!isEqual) {
-        throw new UnauthorizedException('Password does not match');
-      }
-      const accessToken = await this.jwtService.signAsync(
-        {
-          sub: user.id,
-          email: user.email,
-        } as ActiveUserData,
-        {
-          audience: this.jwtConfiguration.audience,
-          issuer: this.jwtConfiguration.issuer,
-          secret: this.jwtConfiguration.secret,
-          expiresIn: this.jwtConfiguration.accessTokenTtl,
-        },
-      );
-      return {
-        accessToken,
-      };
+      throw err;
     }
   }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.usersRepository.findOneBy({
+      email: signInDto.email,
+    });
+    if (!user) {
+      throw new UnauthorizedException('User does not exists');
+    }
+    const isEqual = await this.hashingService.compare(
+      signInDto.password,
+      user.password,
+    );
+    if (!isEqual) {
+      throw new UnauthorizedException('Password does not match');
+    }
+    this.generateTokens(user)
+  }
+
+  async generateTokens(user: User) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.signToken<Partial<ActiveUserData>>(
+        user.id,
+        this.jwtConfiguration.accessTokenTtl,
+        { email: user.email },
+      ),
+      this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl),
+    ]);
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async refreshTokens(refreshTokenDto: RefreshTokenDto) {
+    try {
+      const { sub } = await this.jwtService.verifyAsync<
+        Pick<ActiveUserData, 'sub'>
+      >(refreshTokenDto.refreshToken, {
+        secret: this.jwtConfiguration.secret,
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+      });
+      const user = await this.usersRepository.findOneByOrFail({
+        id: sub,
+      });
+      return this.generateTokens(user);
+    } catch (err) {
+      throw new UnauthorizedException();
+    }
+  }
+
+  private async signToken<T>(userId: string, expiresIn: number, payload?: T) {
+    return await this.jwtService.signAsync(
+      {
+        sub: userId,
+        ...payload,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn,
+      },
+    );
+  }
+
+
+}
